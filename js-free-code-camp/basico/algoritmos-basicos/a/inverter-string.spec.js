b6be40685e9f3626a18c083170c778ff21255aa4
Tiago Rios <Tiago.kom@gmail.com>
2022-11-16 21:02:15 -0300

    Projeto js-free-c... movido para repositório free-code-camp

--- js-free-code-camp/basico/algoritmos-basicos/a/inverter-string.spec.js
+++
@@ -1,8 +0,0 @@
-const { inverterString } = require('./inverter-string.js');
-
-test('Deve retornar uma string', () => {
-    expect(typeof inverterString('asdf')).toBe('string');
-});
-test('Deve inverter a string', () => {
-    expect(inverterString('hello world')).toBe('dlrow olleh');
-});
