2eca68f003355473de91ebb36d5f657f9c59b1f0
Tiago Rios <tiago_pc9@hotmail.com>
2023-12-15 18:26:32 -0300

    Mensagens de erro sobre matemática em arquivo separado.

---
+++ src/basico/matematica/matematicaErrorMessages.js
@@ -0,0 +1,9 @@
+const MSG_DIVISAO_POR_ZERO = 'denominador não pode ser igual a zero';
+const MSG_DOIS_OU_MAIS_NUMEROS = 'informe no mínimo dois números';
+const MSG_NUMEROS_OU_STRINGS_NUMERICAS = 'valores devem ser números ou strings numericas';
+
+module.exports = {
+  MSG_DIVISAO_POR_ZERO,
+  MSG_DOIS_OU_MAIS_NUMEROS,
+  MSG_NUMEROS_OU_STRINGS_NUMERICAS,
+}
