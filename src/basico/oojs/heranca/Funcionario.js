fe2b02723ff6e379958aff6b176349602d6ecc2d
Tiago Rios <tiago_pc9@hotmail.com>
2023-12-17 02:01:49 -0300

    Refatoração - Aplicado responsabilidade única.

--- src/basico/oojs/heranca/Funcionario.js
+++ src/basico/oojs/heranca/Funcionario.js
@@ -1,59 +1,7 @@
-/**
- * Função base para demonstrar herança com com prototipagem.
- * 
- * @param {*} nome 
- *      O nome do funcionário. 
- */
-function Funcionario(nome) {
+import departamento from "./departamento";
+
+export default function Funcionario(nome) {
     this.nome = nome || '';
-    this.departamento = 'geral';
+    this.departamento = departamento.GERAL;
 }
-//Definindo uma propriedade dinãmica para uma função acima na cadeia de prototipos.
-Funcionario.prototype.especialidade = 'sem especialidade';
-
-/**
- * Função filha de Funcionário.
- * 
- * @param {*} nome 
- */
-function Operario(nome) {
-    Funcionario.call(this, nome); // Função pai sendo chamada.
-    this.projetos = [];
-}
-Operario.prototype = new Funcionario(); // Parece que com new tem efeitos colaterais..
-
-/**
- * Função filha de Operário.
- * 
- * @param {*} nome 
- * @param {*} maquina 
- */
-function Engenheiro(nome, maquina) {
-    Operario.call(this, nome); // Função pai
-    this.departamento = 'engenharia';
-    this.maquina = maquina || 'retro-escavadeira'; // Específico para engenheiros
-}
-Engenheiro.prototype = Object.create(Operario.prototype);
-
-/**
- * Função filha de Operário.
- * 
- * Esta função não é vinculada ao prototipo da função pai. Ela quebra a cadeia.
- * As propriedades adicionadas dinamicamente na cadeia de prototipos não são 
- * herdadas por esta.
- * 
- * @param {*} nome 
- */
-function Vendedor(nome) {
-    Operario.call(this, nome); // Função pai sendo chamada.
-    this.departamento = 'vendas'
-    this.quota = 100; // Específico para vendedores
-}
-// Vendedor.prototype = new Operario(); // Não vinculada a cadeia de prototipos.
-
-module.exports = {
-    Funcionario,
-    Operario,
-    Vendedor,
-    Engenheiro,
-}
+Funcionario.prototype.especialidade = 'especialidade';
--- src/basico/oojs/heranca/Funcionario.spec.js
+++ src/basico/oojs/heranca/Funcionario.spec.js
@@ -1,86 +1,32 @@
-const { Operario, Vendedor, Engenheiro, Funcionario } = require('./Funcionario.js');
+import Funcionario from './Funcionario.js';
 
-let vendedor, operario, engenheiro, funcionario, funcionario2;
+const departamento = require('./departamento');
+const pessoaMock = require('./pessoaMock');
 
-const NOME_FUNCIONARIO = 'nome do funcionario',
-    NOME_VENDEDOR = 'nome do vendedor',
-    NOME_OPERARIO = 'nome do operario',
-    NOME_ENGENHEIRO = 'nome do engenheiro',
+const funcionario = new Funcionario(pessoaMock.nome)
 
-    DEPARTAMENTO_GERAL = 'geral',
-    DEPARTAMENTO_VENDEDOR = 'vendas',
-    DEPARTAMENTO_ENGENHEIRO = 'engenharia',
+test(`deve retornar nome do funcionário`, () => {
+    expect(funcionario.nome).toBe(pessoaMock.nome);
+})
 
-    NOME_MAQUINA = 'retro-escavadeira';
+test(`deve retornar departamento do funcionário`, () => {
+    expect(funcionario.departamento).toBe(departamento.GERAL);
+})
 
-beforeAll(() => {
-    vendedor = new Vendedor(NOME_VENDEDOR);
-    operario = new Operario(NOME_OPERARIO);
-    engenheiro = new Engenheiro(NOME_ENGENHEIRO);
-    funcionario = new Funcionario(NOME_FUNCIONARIO);
-    funcionario2 = new Funcionario();
-});
+test(`deve retornar especialidade default do funcionario`, () => {
+    expect(funcionario.especialidade).toBe("especialidade");
+})
 
-describe('orientação objetos em js', () => {
-    describe('funcionário', () => {
-        test(`deve definir nome do funcionário passado no construtor`, () => {
-            expect(funcionario.nome).toBe(NOME_FUNCIONARIO);
-        });
+describe('propriedade __proto__ da cadeia de prototypes', () => {
+    test(`funcionario com 1 propriedade __proto__`, () => {
+        expect(funcionario.__proto__).toBe(Funcionario.prototype);
+    })
 
-        test(`deve usar o nome default do objeto funcionário`, () => {
-            expect(funcionario2.nome).toBe('');
-        });
-    });
+    test(`funcionario com 2 propriedades __proto__`, () => {
+        expect(funcionario.__proto__.__proto__).toBe(Object.prototype);
+    })
 
-    describe('operario', () => {
-        test(`herança de operario e funcionario`, () => {
-            expect(operario.nome).toBe(NOME_OPERARIO);
-            expect(operario.departamento).toBe(DEPARTAMENTO_GERAL);
-            expect(operario.projetos.length).toBe(0);
-            expect(operario.especialidade).toBe('sem especialidade');
-
-        });
-
-        test(`operario deve herda propriedade dinãmica`, () => {
-            expect(operario.especialidade).toBe('sem especialidade');
-        });
-    });
-
-    describe('vendedor', () => {
-        test(`herança de vendedor e operario`, () => {
-            expect(vendedor.nome).toBe(NOME_VENDEDOR);
-            expect(vendedor.quota).toBe(100);
-            expect(vendedor.departamento).toBe(DEPARTAMENTO_VENDEDOR);
-            expect(vendedor.projetos.length).toBe(0);
-        });
-
-        test(`vendedor não deve herda propriedade dinãmica`, () => {
-            expect(vendedor.especialidade).toBe(undefined);
-        });
-    });
-
-    describe('engenheiro', () => {
-        test(`herança de engenheiro e operario`, () => {
-            expect(engenheiro.nome).toBe(NOME_ENGENHEIRO);
-            expect(engenheiro.maquina).toBe(NOME_MAQUINA);
-            expect(engenheiro.departamento).toBe(DEPARTAMENTO_ENGENHEIRO);
-            expect(engenheiro.projetos.length).toBe(0);
-
-            expect(engenheiro.especialidade).toBe('sem especialidade');
-        });
-
-        test(`engenheiro deve herdar propriedade dinãmica`, () => {
-            expect(engenheiro.especialidade).toBe('sem especialidade');
-        });
-    });
-
-    describe('propriedade __proto__ da cadeia de prototypes', () => {
-        test(`verificar propriedade __proto__`, () => {
-            expect(engenheiro.__proto__).toBe(Engenheiro.prototype);
-            expect(engenheiro.__proto__.__proto__).toBe(Operario.prototype);
-            expect(engenheiro.__proto__.__proto__.__proto__).toBe(Funcionario.prototype);
-            expect(engenheiro.__proto__.__proto__.__proto__.__proto__).toBe(Object.prototype);
-            expect(engenheiro.__proto__.__proto__.__proto__.__proto__.__proto__).toBe(null);
-        });
-    });
-});
+    test(`funcionario com 3 propriedades __proto__`, () => {
+        expect(funcionario.__proto__.__proto__.__proto__).toBe(null);
+    })
+})
